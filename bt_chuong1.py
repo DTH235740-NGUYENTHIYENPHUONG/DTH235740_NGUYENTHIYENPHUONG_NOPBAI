dbc277ecfd5d60db210293b866bb9417f49189f0
DTH235740-NGUYENTHIYENPHUONG <pn9694851@gmail.com>
2025-08-26 10:48:39 +0700

    Add files via upload

---
+++ bt_chuong1.py
@@ -0,0 +1,51 @@
+﻿# vidu chuong 1
+# vidu 1
+print('Albert Einstein đã từng nói: <Một người không bao giờ mắc sai lầm không bao giờ thử bất kỳ điều gì mới.\n')
+# vidu 2
+if 5 > 2:
+        print('5 lớn hơn 2\n')
+
+if True:
+    print("true\n")
+else:
+    print("false\n")
+
+# baitap chuong 1
+
+#baitap 4
+print('Chao cac ban!\n')
+
+#baitap 5
+print("Tên tôi là \"Nguyễn Thị Yến Phương\".\n")
+
+#baitap 6
+print('Mình về mình có nhớ ta?')
+print('Mười lăm năm ấy thiết tha mặn nồng.')
+print('Mình về mình có nhớ không?')
+print('Nhìn cây nhớ núi, nhìn sông nhớ nguồn.')
+
+#baitap 7
+nhap = input("\nMời bạn nhập chuỗi bất kỳ: ")
+print("Chuỗi bạn vừa nhập là: ", nhap, '\n')
+
+#baitap 8
+print('****')
+print('*  *')
+print('*  *')
+print('****\n')
+
+#baitap 9
+print('*********')
+print('*********')
+print('*********')
+print('*********\n')
+
+#baitap 10
+print('     *')
+print('    ***')
+print('  *******')
+print('    ***  ')
+print('   *****    ')
+print('***********')
+print('    * *')
+print('    * *\n')
