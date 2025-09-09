6b63ef747938d52902c2bec4cd143ea0a945b494
DTH235740_NGUYENTHIYENPHUONG <phuong_dth235740@student.agu.edu.vn>
2025-09-09 10:33:13 +0700

    update bt_chuong2

---
+++ bt_chuong2/bai3.py
@@ -0,0 +1,7 @@
+print("---Tính điểm trung bình của một học sinh---")
+toan=float(input("Nhập điểm Toán:"))
+ly=float(input("Nhập điểm lý:"))
+hoa=float(input("Nhập điểm hóa:"))
+dtb = (toan+ly+hoa)/3
+print("Điểm tring binh của học sinh la: ",dtb)
+print("Điểm đã làm tròn: ", round(dtb,2))
